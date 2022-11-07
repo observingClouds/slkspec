6c1e5a29ae3b1cae7021cda0de90dfc40e7635c6
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-11-07 20:28:53 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ import versioneer
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as f:
+    requirements = f.read().strip().split("\n")
+
 setuptools.setup(
     name="slkspec",
     version=versioneer.get_version(),
@@ -22,10 +25,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
-    install_requires=[
-        "fsspec>=0.9.0",
-        "aiohttp",
-    ],
+    install_requires=requirements,
     entry_points={
         "fsspec.specs": [
             "slk=slkspec.SLKFileSystem",
