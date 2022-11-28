3c2292b28e6504c163fc193c39a68a1b916e39d7
k204230 <bergemann@dkrz.de>
2022-11-28 23:14:03 +0100

    Add mypy type checking

--- slkspec/core.py
+++ slkspec/core.py
@@ -1,43 +1,40 @@
 from __future__ import annotations
+from collections import defaultdict
+from getpass import getuser
 import logging
 import io
 import os
 from pathlib import Path
 from queue import Queue
+import re
 import threading
-from typing import AnyStr, Any, BinaryIO, IO, Literal, Optional, TextIO, Union
+from typing import (
+    AnyStr,
+    Any,
+    Dict,
+    IO,
+    Literal,
+    List,
+    Optional,
+    overload,
+    Tuple,
+    TypedDict,
+    Union,
+)
+import warnings
 
 from fsspec.spec import AbstractFileSystem
+from pyslk import pyslk
 
-# from pyslk import pyslk as pslk
 
 logger = logging.getLogger("slkspec")
+logger.setLevel(logging.INFO)
+
 
 MAX_RETRIES = 2
-FileQueue: Queue[str] = Queue(maxsize=-1)
+FileQueue: Queue[Tuple[str, str]] = Queue(maxsize=-1)
+FileInfo = TypedDict("FileInfo", {"name": str, "size": Literal[None], "type": str})
 _retrieval_lock = threading.Lock()
-
-
-class pyslk:
-    @staticmethod
-    def slk_list(inp: str | Path) -> str:
-        from subprocess import run, CalledProcessError, PIPE
-
-        return run(
-            ["ls", "-l", f"{Path(inp).expanduser().absolute()}"],
-            stdout=PIPE,
-            stderr=PIPE,
-        ).stdout.decode()
-
-    @staticmethod
-    def slk_retrieve(items: list[str]) -> None:
-        import shutil
-        import time
-
-        print(f"Retrieving {len(items)} items from tape")
-        time.sleep(len(items) / 2)
-        # Path(out).parent.mkdir(exist_ok=True, parents=True)
-        # shutil.copy(inp, out)
 
 
 class SLKFile(io.IOBase):
@@ -45,17 +42,40 @@ class SLKFile(io.IOBase):
 
     Parameters
     ----------
-    file: str | pathlib.Path
-        path to the files.
+    url: str
+        Source path of the file that should be retrieved.
+    local_file: str
+        Destination path of the downloaded file.
+    override: bool, default: False
+        Override existing files
+    touch: bool, default: True
+        Update existing files on the temporary storage to prevent them
+        from being deleted.
     mode: str, default: rb
-        specify the mode in which the files are opened
+        Specify the mode in which the files are opened
 
         'r'       open for reading (default)
-        'b'       binary mode
-        't'       text mode (default)
-
-    **kwargs
+        'b'       binary mode (default)
+        't'       text mode
+    file_permissions: int, default 0o3777
+        Permission when creating directories and files.
+    **kwargs:
         Additional keyword arguments passed to the open file descriptor method.
+
+    Example
+    -------
+
+    Use fsspec to open data stored on tape, temporary data will be downloaded
+    to a central scratch folder:
+
+    ::
+
+        import ffspec
+        import xarray as xr
+
+        url = fsspec.open("slk:////arch/bb1203/data.nc", slk_cache="/scratch/b/b12346").open()
+        dset = xr.open_dataset(url)
+
     """
 
     write_msg: str = "Write mode is not suppored"
@@ -64,16 +84,16 @@ class SLKFile(io.IOBase):
 
     def __init__(
         self,
-        url: str | Path,
-        local_file: str | Path,
+        url: str,
+        local_file: str,
         *,
         override: bool = True,
         mode: str = "rb",
         touch: bool = True,
-        file_mod: int = 0o3777,
+        file_permissions: int = 0o3777,
         _lock: threading.Lock = _retrieval_lock,
-        _file_queue: Queue[tuple[str, str]] = FileQueue,
-        **kwargs,
+        _file_queue: Queue[Tuple[str, str]] = FileQueue,
+        **kwargs: Any,
     ):
         if not set(mode) & set("r"):  # The mode must have a r
             raise NotImplementedError(self.write_msg)
@@ -82,7 +102,7 @@ class SLKFile(io.IOBase):
         self._file = str(Path(local_file).expanduser().absolute())
         self._url = str(url)
         self.touch = touch
-        self.file_mod = file_mod
+        self.file_permissions = file_permissions
         self._order_num = 0
         self._file_obj: Optional[IO[Any]] = None
         self._lock = _lock
@@ -97,6 +117,8 @@ class SLKFile(io.IOBase):
             if not Path(self._file).exists() or override:
                 self._file_queue.put((self._url, str(Path(self._file).parent)))
             elif Path(self._file).exists():
+                if self.touch:
+                    Path(self._file).touch()
                 self._file_obj = open(self._file, mode, **kwargs)
 
     @property
@@ -108,16 +130,24 @@ class SLKFile(io.IOBase):
 
     def _retrieve_items(self, retrieve_files: list[tuple[str, str]]) -> None:
         """Get items from the tape archive."""
-        import shutil
 
-        print(f"Retrieving {len(retrieve_files)} items from tape")
+        retrieval_requests: Dict[Path, List[str]] = defaultdict(list)
+        logger.critical("Retrieving %i items from tape", len(retrieve_files))
         for inp_file, out_dir in retrieve_files:
-            Path(out_dir).mkdir(
-                exist_ok=True, parents=True, mode=self.file_mod
-            )
-            target = Path(out_dir) / Path(inp_file).name
-            shutil.copy(inp_file, target)
-            target.chmod(self.file_mod)
+            retrieval_requests[Path(out_dir)].append(inp_file)
+        for output_dir, inp_files in retrieval_requests.items():
+            output_dir.mkdir(parents=True, exist_ok=True, mode=self.file_permissions)
+            logger.critical("Creating slk query for %i files", len(inp_files))
+            search_str = pyslk.slk_search(pyslk.slk_gen_file_query(inp_files))
+            search_id_re = re.search("Search ID: [0-9]*", search_str)
+            if search_id_re is None:
+                raise ValueError("No files found in archive.")
+            search_id = int(search_id_re.group(0)[11:])
+            logger.critical("Retrieving files for search id: %i", search_id)
+            pyslk.slk_retrieve(search_id, str(output_dir))
+            logger.critical("Adjusting file permissions")
+            for out_file in map(Path, inp_files):
+                out_file.chmod(self.file_permissions)
 
     def _cache_files(self) -> None:
         with self._lock:
@@ -142,11 +172,11 @@ class SLKFile(io.IOBase):
         if self._file_obj is not None:
             return self._file_obj.tell()
         self._cache_files()
-        return self._file_obj.tell()
+        return self._file_obj.tell()  # type: ignore
 
-    def seek(self, target: int) -> int:
+    def seek(self, target: int) -> int:  # type: ignore
         if self._file_obj is None:
-            return self._file_obj.seek(target)
+            return self._file_obj.seek(target)  # type: ignore
         self._cache_files()
         return self._file_obj.seek(target)
 
@@ -174,18 +204,18 @@ class SLKFile(io.IOBase):
         """
         if self._file_obj is None:
             self._cache_files()
-        return self._file_obj.read(size)
+        return self._file_obj.read(size)  # type: ignore
 
     @staticmethod
     def flush() -> None:
         """Flushing file systems shouldn't work for ro modes."""
         return None
 
-    def writelines(self, *arg) -> None:
+    def writelines(self, *arg: Any) -> None:
         """Comptibility method."""
         raise NotImplementedError(self.write_msg)
 
-    def write(self, *arg) -> None:
+    def write(self, *arg: Any) -> None:
         """Writing to tape is not supported."""
         raise NotImplementedError(self.write_msg)
 
@@ -195,18 +225,41 @@ class SLKFile(io.IOBase):
 
 
 class SLKFileSystem(AbstractFileSystem):
+    """Abstract class for hsm files systems.
+
+    The implementation intracts with the hsm tape storage system, files
+    that are accessed are downloaded to a temporary data storage.
+
+    Parameters
+    ----------
+
+    slk_cache: str | Path, default: None
+        Destination of the temporary storage. This directory is used to
+        retrieve data from tape.
+    block_size: int, default: None
+         Some indication of buffering - this is a value in bytes
+    file_permissions: int, default: 0o3777
+        Permission when creating directories and files.
+    override: bool, default: False
+        Override existing files
+    touch: bool, default: True
+        Update existing files on the temporary storage to prevent them
+        from being deleted.
+    **storage_options:
+        Additional options passed to the AbstractFileSystem class.
+    """
+
     protocol = "slk"
     sep = "/"
 
     def __init__(
         self,
-        auth=None,
-        block_size=None,
-        local_cache=None,
+        block_size: Optional[int] = None,
+        slk_cache: Optional[Union[str, Path]] = None,
+        file_permissions: int = 0o3777,
         touch: bool = True,
         override: bool = False,
-        client_kwargs=None,
-        **storage_options,
+        **storage_options: Any,
     ):
         super().__init__(
             block_size=block_size,
@@ -214,77 +267,87 @@ class SLKFileSystem(AbstractFileSystem):
             loop=None,
             **storage_options,
         )
-        local_cache = local_cache or os.environ.get("SLK_CACHE")
-        if not local_cache:
-            raise KeyError(
-                "Local cache should be given. As an alternative set environment variable SLK_CACHE."
+        slk_cache = slk_cache or os.environ.get("SLK_CACHE")
+        if not slk_cache:
+            local_cache = f"/scratch/{getuser()[0]}/{getuser()}"
+            warnings.warn(
+                "The slk_cache variable nor the SLK_CACHE environment"
+                "variable wasn't set. Falling back to default "
+                f"{local_cache}",
+                UserWarning,
+                stacklevel=2,
             )
         self.touch = touch
-        self.local_cache = Path(local_cache)
-        self.client_kwargs = client_kwargs or {}
+        self.slk_cache = Path(local_cache)
         self.override = override
+        self.file_permissions = file_permissions
 
-    def ls(self, path, detail=True, **kwargs):
-        filelist = pyslk.slk_list(path).split("\n")
-        detail_list = []
+    @overload
+    def ls(
+        self, path: Union[str, Path], detail: Literal[True], **kwargs: Any
+    ) -> List[FileInfo]:
+        ...
+
+    @overload
+    def ls(
+        self, path: Union[str, Path], detail: Literal[False], **kwargs: Any
+    ) -> List[str]:
+        ...
+
+    def ls(
+        self, path: Union[str, Path], detail: bool = True, **kwargs: Any
+    ) -> Union[List[FileInfo], List[str]]:
+        """List objects at path.
+
+        This includes sub directories and files at that location.
+
+        Parameters
+        ----------
+        path: str | pathlib.Path
+            Path of the file object that is listed.
+        detail: bool, default: True
+            if True, gives a list of dictionaries, where each is the same as
+            the result of ``info(path)``. If False, gives a list of paths
+            (str).
+
+
+        Returns
+        -------
+        list : List of strings if detail is False, or list of directory
+               information dicts if detail is True.
+        """
+        path = Path(path)
+        filelist = pyslk.slk_list(str(path)).split("\n")
+        detail_list: List[FileInfo] = []
         types = {"d": "directory", "-": "file"}
         for file_entry in filelist[:-2]:
-            entry = {
-                "name": path
-                + "/"
-                + file_entry.split(" ")[
-                    -1
-                ],  # this will fail if filenames contains whitespaces
+            entry: FileInfo = {
+                "name": str(path / " ".join(file_entry.split()[8:])),
                 "size": None,  # sizes are human readable not in bytes
                 "type": types[file_entry[0]],
             }
             detail_list.append(entry)
         if detail:
             return detail_list
-        else:
-            return [d["name"] for d in detail_list]
-
-    def cat_file(self, path, start=None, end=None, touch=False, mod="3o777"):
-        """Get file content
-
-        Inputs
-        ------
-        path : str
-          file to open
-        start : int
-          seek position within file, optional
-        end : int
-          end position of file read, optional
-        touch : bool
-          switch to update file modification and access times of local
-          files at every request. This option might be helpful to
-          keep retrievals from tape to a minimum and prevent often
-          used files from being garbage collected.
-
-        Returns
-        -------
-        - bytes if start and end are given
-        - file object otherwise
-        """
-        slk_file = self._open(path, mode="rb")
-        if isinstance(start, int) and isinstance(end, int):
-            return slk_file.seed(end - start)
-        return slk_file
+        return [d["name"] for d in detail_list]
 
     def _open(
         self,
         path: str | Path,
         mode: str = "rb",
-        encoding: Optional[str] = None,
-        **kwargs,
-    ):
+        block_size: Optional[int] = None,
+        autocommit: bool = True,
+        cache_options: Optional[Dict[str, Any]] = None,
+        **kwargs: Any,
+    ) -> SLKFile:
         path = Path(self._strip_protocol(path))
-        local_path = self.local_cache.joinpath(*path.parts[1:])
+        local_path = self.slk_cache.joinpath(*path.parts[1:])
         return SLKFile(
-            path,
-            local_path,
+            str(path),
+            str(local_path),
             mode=mode,
             override=self.override,
             touch=self.touch,
-            encoding=encoding,
+            encoding=kwargs.get("encoding"),
+            file_permissions=self.file_permissions,
         )
