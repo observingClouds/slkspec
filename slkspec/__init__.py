6836539ee5d6312866cb66cba0e1705cd957fb3c
Dogus Kaan Bilir <bilir@dkrz.de>
2025-02-21 14:09:05 +0100

    fix: resolve unintended import from importlib.metadata

--- slkspec/__init__.py
+++ slkspec/__init__.py
@@ -1,9 +1,10 @@
+import importlib.metadata
+
 from fsspec import register_implementation
 
-from importlib.metadata import version
 from .core import SLKFileSystem, logger
 
-__version__ = version("slkspec")
+__version__ = importlib.metadata.version("slkspec")
 
 register_implementation(SLKFileSystem.protocol, SLKFileSystem)
 
